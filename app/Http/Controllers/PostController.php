d7ccd400ca4b6fc6f76dd5ba305cdaac15302197
F3licia <romeofelicia@gmail.com>
2021-07-01 21:15:36 +0200

    index post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post; 
 
 class PostController extends Controller
 {
-    //
+    function index() {
+        $posts = post::all();
+
+        return view('posts.index', ['posts'=> $posts]);
+    }
 }
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,3 @@
+@if(count($posts) == 0)
+  <h3>Il database è vuoto.</h3>
+@endif
