189d58fa21bec922589644a73437db6b449ae602
F3licia <romeofelicia@gmail.com>
2021-07-04 17:03:16 +0200

    aggiunte categorie e category_id a posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,18 +5,18 @@
 @section('content')
 
 <div class="container">
-<div class="d-flex align-content-between flex-wrap">
+<div class="card-group">
     
     @foreach($posts as $post)
-        <div class="card mb-auto p-2" style="width: 18rem;">
-            <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
+        <div class="card mb-auto p-2" style="width: 18rem">
+            <img class="card-img-top" src="https://picsum.photos/200/100" alt="Card image cap">
             <div class="card-body">
-                <h3 class="card-title">{{$post->title}}</h3>
+                <h2 class="card-title text-uppercase">{{$post->title}}</h2>
                 <h4>By {{$post->user->name}}</h4>
                 <p>On {{ $post->created_at->format('d M Y - H:i:s') }}</p>
-                <h3 class="card-title">{{ substr($post->content, 0, 50)."..."}}</h3>
+                <h3 class="card-text">{{ substr($post->content, 0, 50)."..."}}</h3>
                 
-                <div class="d-flex justify-content-center align-items-start">
+                <div class="d-flex justify-content-center align-items-start"> 
                     <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary"> More </a>
                     <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-primary"> Edit </a> 
                     @include('layouts.partials.deleteBtn', [ "id" => $post->id, "resource" => "posts" ])
